452aaf954b19bb85bd8526526c4f23c0bb41ed17
kennobith <97459261+kennobith@users.noreply.github.com>
2026-03-09 13:13:10 -0300

    update pagina de descuentos

--- asistencias_assistant/pages/3_✅_Descuentos.py
+++ asistencias_assistant/pages/3_✅_Descuentos.py
@@ -1,5 +1,62 @@
 import streamlit as st
+import pandas as pd
+import io
+import re
 
-st.set_page_config(page_title="Descuentos", page_icon="✅")
+from services.descuentos import leer_excel
+from services.descuentos import armado_df
+from services.descuentos import comparacion
+from services.descuentos import hay_descuentos_vacios
+from services.descuentos import imprimir_diferencias
+from services.descuentos import unir_diccionarios
+from services.descuentos import crear_excel
 
-st.subheader("En construcción 🚧")
+st.set_page_config(page_title="Descuentos", page_icon="✅",  layout = 'wide')
+
+st.markdown("**Versión beta: cualquier cosa rara que encuentres, no dudes en reportarla!**")
+
+st.markdown("Subí la planilla de la dependencia directo del Google Sheets.")
+archivo_izquierda = st.file_uploader("", type = 'xlsx', accept_multiple_files = False, key = "archivo_izquierda")
+st.markdown("Subí la planilla del segundo cálculo de descuentos.")
+archivo_derecha = st.file_uploader("", type = 'xlsx', accept_multiple_files = False, key = "archivo_derecha")
+
+if archivo_derecha and archivo_izquierda:
+
+    excel_file = pd.ExcelFile(archivo_izquierda)
+    nombres_hojas = excel_file.sheet_names
+    opciones_hojas = [""] + nombres_hojas
+
+    hoja = st.selectbox(
+    "Elegir hoja que se quiere procesar",
+    opciones_hojas
+    )
+
+    if hoja:
+        df_uno = leer_excel(archivo_izquierda, hoja)
+        df_dos = leer_excel(archivo_derecha)
+
+        dic_legajos_izq, df_limpio_izq = armado_df(df_uno)
+        dic_legajos_der, df_limpio_der = armado_df(df_dos)
+
+        if not hay_descuentos_vacios(df_limpio_izq):
+            diferencias = comparacion(df_limpio_izq,df_limpio_der)
+
+        else:
+            st.write("No se puede procesar la planilla porque existen campos vacios en la columna de \"Se descuenta?\"")
+            st.stop()
+
+        print("-----DIFERENCIAS--------")
+        imprimir_diferencias(diferencias)
+        print("------------------------")
+        dict_personas = unir_diccionarios(dic_legajos_izq, dic_legajos_der)
+        nombre_dependencia = re.sub(r"\s*\(\d+\)", "", archivo_izquierda.name)
+        buffer = io.BytesIO()
+        planilla_final = crear_excel(df_limpio_izq, diferencias, dict_personas, buffer)
+        buffer.seek(0)
+        st.download_button(
+            label="Descargar planilla para comparar",
+            data=buffer,
+            file_name=nombre_dependencia,
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            icon=":material/download:",
+        )
