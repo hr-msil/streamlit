1501c9f03aaab2ce57535e99c2bc30bbd4fffc92
milagrosmaurer <112497616+milagrosmaurer@users.noreply.github.com>
2026-03-10 10:15:50 -0300

    Add files via upload

--- despacho/pages/2_📎_The_Annex_App.py
+++ despacho/pages/2_📎_The_Annex_App.py
@@ -6,33 +6,58 @@ from docx.enum.section import WD_ORIENT
 from docx.shared import Mm
 import streamlit as st
 from io import BytesIO
+import xlrd
 
 from theannexapp import armar_anexos
-
-
-## Streamlit APP ## 
+from theannexapp import armar_anexos_dos
 
 st.title('The Annex App📎')
-st_archivos = st.file_uploader("Clickeá donde dice 'Browse files' y subí los anexos", type = ["xls"], accept_multiple_files=True)
-st.write("Recordá que los archivos tienen que tener como nombre el expediente al que refieren.")
 
-if st_archivos:
-    st.success(f"Subiste {len(st_archivos)} planilla(s)")
-    titulo = st.text_input("Escribí el nombre del archivo y presioná Enter", "Anexo Subsecretaría ABC")
+## Streamlit APP ## 
+opciones = [" ","Hacer anexo de mensualizados", "Hacer el otro anexo"]
 
-    if st.button("Procesar y armar anexos"):
-        documento = armar_anexos(st_archivos)
+opcion = st.selectbox(
+    "Elegir una opción",
+    opciones
+)
 
-        buffer = BytesIO()
-        documento.save(buffer)
-        buffer.seek(0)
+if opcion == " ":
+    st.subheader("IMPORTANTE❗: seleccionar el área antes de continuar")
+else:
 
-        st.info('Recordá revisar el documento')
-        st.download_button(
-            label="Descargar notificaciones",
-            data=buffer,
-            file_name= titulo.strip() + ".docx",
-            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            icon=":material/download:",
+    st_archivos = st.file_uploader("Clickeá donde dice 'Browse files' y subí los archivos", accept_multiple_files=True)
 
-        )
+    if st_archivos:
+        st.success(f"Subiste {len(st_archivos)} planilla(s)")
+        titulo = st.text_input("Escribí el nombre del archivo y presioná Enter", "Anexo Subsecretaría ABC")
+
+        if opcion == "Hacer anexo de mensualizados":
+            documento = armar_anexos(st_archivos)
+
+            buffer = BytesIO()
+            documento.save(buffer)
+            buffer.seek(0)
+
+            st.info('Recordá revisar el documento')
+            st.download_button(
+                label="Descargar notificaciones",
+                data=buffer,
+                file_name= titulo.strip() + ".docx",
+                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                icon=":material/download:",
+            )
+        else:
+            documento = armar_anexos_dos(st_archivos)
+
+            buffer = BytesIO()
+            documento.save(buffer)
+            buffer.seek(0)
+
+            st.info('Recordá revisar el documento')
+            st.download_button(
+                label="Descargar notificaciones",
+                data=buffer,
+                file_name= titulo.strip() + ".docx",
+                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                icon=":material/download:",
+            )
