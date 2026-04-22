02bee494bc600622710eb8fc7cb71317c7d68557
milagrosmaurer <112497616+milagrosmaurer@users.noreply.github.com>
2026-04-22 09:59:01 -0300

    Update mensualizados.py

--- mensualizados/mensualizados.py
+++ mensualizados/mensualizados.py
@@ -156,7 +156,7 @@ elif opcion == "AMBIENTE Y ESPACIO PUBLICO" or opcion == "DESARROLLO HUMANO Y DE
             for df_oficina in df_oficinas:
                 
 
-                df_oficina = df_oficina[(df_oficina["Evaluación"] != "Enviar nota de designación") & (~df_oficina["Fecha Egreso Cargo"].astype(str).str.contains(pattern, na=False)) & (~df_oficina["Fecha Egreso Cargo"].astype(str) == "Art. 32")]
+                df_oficina = df_oficina[(df_oficina["Evaluación"] != "Enviar nota de designación") & (~df_oficina["Fecha Egreso Cargo"].astype(str).str.contains(pattern, na=False)) & (df_oficina["Fecha Egreso Cargo"].astype(str) != "Art. 32")]
                 if df_oficina.shape[0] == 0: continue
                 df_oficina = borrar_ultimas_columnas(df_oficina,5)
                 df_oficina = df_oficina.reset_index(drop=True)
@@ -280,7 +280,7 @@ else:
                     
 
                     df_oficina = df_oficina.reset_index(drop=True)
-                    df_oficina = df_oficina[(df_oficina["Evaluación"] != "Enviar nota de designación") & (~df_oficina["Fecha Egreso Cargo"].astype(str).str.contains(pattern, na=False)) & (~df_oficina["Fecha Egreso Cargo"].astype(str) == "Art. 32")]
+                    df_oficina = df_oficina[(df_oficina["Evaluación"] != "Enviar nota de designación") & (~df_oficina["Fecha Egreso Cargo"].astype(str).str.contains(pattern, na=False)) & (df_oficina["Fecha Egreso Cargo"].astype(str) != "Art. 32")]
                     if df_oficina.shape[0] == 0: continue
                     oficina = df_oficina["Oficina"].unique()  # Array de valores únicos
                     df_oficina = borrar_ultimas_columnas(df_oficina,5)
