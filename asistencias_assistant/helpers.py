ac1ffc3de848a536111677209c85ae1657db762c
milagrosmaurer <112497616+milagrosmaurer@users.noreply.github.com>
2026-04-28 13:00:43 -0300

    Add files via upload

--- asistencias_assistant/helpers.py
+++ asistencias_assistant/helpers.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 from pandas.api.types import is_numeric_dtype
 import re
 import numpy as np
+import json
+from pathlib import Path
 
 codigos_ausencias_no_descontables_viaticos = set([0,18,38,64,66,76,78,79,80,81,87,100,101,102,600])
 codigos_ausencias_no_descontables_hhee = (set([1,4,5,6,7,8,9,10,11,12,13,
@@ -23,6 +25,24 @@ codigos_ausencias_no_descontables_hhee = (set([1,4,5,6,7,8,9,10,11,12,13,
 #################################
 # FUNCIONES INTERNAS HELPERS.PY #
 #################################
+
+def obtener_dias_feriados(anio: int, mes: int) -> list[int]:
+    #BASE_DIR = Path(__file__).resolve().parent.parent
+    #ruta_feriados = BASE_DIR / "feriados.txt"
+    ruta_feriados = "feriados.txt"
+    with open(ruta_feriados, encoding="utf-8") as f: 
+        texto = f.read().replace("NaN", "null")
+    
+    feriados = json.loads(texto)
+    dias = []
+
+    for fdo in feriados:
+        fecha = datetime.strptime(fdo["fecha"], "%Y-%m-%d")
+
+        if fecha.year == anio and fecha.month == mes:
+            dias.append(fecha.day)
+
+    return sorted(dias)
 
 # Se usa en chequeo_legajos y en armado_csv
 def procesar_oficinas(oficinas):
@@ -326,5 +346,68 @@ def lista_a_string(lista):
         s += ", " + str(x)
 
     return s 
+
+def tipo_de_fila_cl(fila:pd.Series) -> tuple[int,int]:
+    '''
+    Dado una fila del documento otorgado por las oficinas de HHEE determinamos si las filas nos dicen el número de la oficina, o en caso conttrario
+    los  datos de la persona.
+    
+    :param fila: Fila correspondiente al dataFrame de HHEE
+    :type fila: pd.Series
+    :return: Una tupla de int, donde el primer int corresponde al tipo de fila (0:oficina, 1:persona, 2:oficina(año != 2026)), el segundo int corresponde al dato de interés
+    :rtype: tuple[int, int]
+    '''
+
+    if fila["Legajo"] == "OFICINA: ":
+
+        if fila["Nombre"] == 2026:
+
+            return 0, fila["Oficina"]
+        
+        else:
+
+            return 2,0 #En caso de que no corresponda al año 2026, retornamos 2 y 0
+    
+    else:
+
+        return 1, fila["Legajo"]
+
+def crear_df(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Recorremos todas las filas del dataFrame de legajos por oficina para armarlo con el legajo en la primer columna, y en la segunda columna la oficina la cual 
+    pertenece ese legajo, luego convertimos a int el número de oficina y el legajo
+    
+    :param df: dataFrame de legajos por oficina
+    :type df: pd.DataFrame
+    :return: dataFrame con los legajos en primer columna y las oficinas en la segunda columna
+    :rtype: DataFrame
+    '''
+    cant_filas = df.shape[0]
+    
+    legajos = []
+    oficinas = []
+    oficina_actual = 0
+
+    for i in range(cant_filas):
+
+        fila = df.iloc[i]
+
+        tipo, dato = tipo_de_fila_cl(fila)
+
+        if tipo == 0:
+            oficina_actual = dato
+        elif tipo == 1:
+            legajos.append(dato)
+            oficinas.append(oficina_actual)
+    #Si la fila es de tipo 2 la ignoramos, no corresponde a este año
+        
+    df_res = pd.DataFrame({"Legajo": legajos, "Oficina": oficinas})
+    df_res = df_res[df_res["Oficina"] != 0] #Filtramos porque los legajos no correspondientes al 2026 me quedaron con numero de oficina 0 (VER)
+    #Lo que pasa es que sigo iterando sobre las personas, lo unico que ignoro es el numero de  oficina, como todos los años que no son 2026 están al principio
+    # del dataFrame, me quedan los legajos con número de oficina 0
+    df_res["Legajo"] = df_res["Legajo"].astype('Int64')
+    df_res["Oficina"] = df_res["Oficina"].astype('Int64')
+
+    return df_res
 
 
