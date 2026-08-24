064274b6e1327eee98e0ca302b821435023fb8b8
milagrosmaurer <milagros.maurer@gmail.com>
2026-08-24 14:47:35 -0300

    agrego mecanizada a menu principal

--- haberes/👋_Hola.py
+++ haberes/👋_Hola.py
@@ -10,11 +10,12 @@ st.write("# Haberes! 👋")
 
 st.sidebar.success("Seleccionar una página arriba")
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 
 col1.page_link("pages/1_📊_Control_productividades.py", label="Para el control de Productividades", icon="📊")
 col2.page_link("pages/2_📈_Permisos_de_cobro .py", label="Para los permisos de cobro", icon="📈")
 col3.page_link("pages/3_🗞️_Extra_Extra.py", label="Extra Extra", icon="🗞️")
+col4.page_link("pages/4_⚙️_Mecanizadas.py", label = "Mecanizadas", icon = "⚙️")
 
 
 
